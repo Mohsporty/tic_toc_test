c69286cb59005108cdf7b40c91c68fd736973432
Mohsporty <m.alassdi@gmail.com>
2025-01-27 17:31:21 +0300

    uploaded

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -58,8 +58,70 @@ class _HomeScreenState extends State<HomeScreen> {
                   hintText: "Player 1 Name",
                   hintStyle: TextStyle(color: Colors.white),
                 ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Plases Enter Player 1 Name ";
+                  }
+                  return null;
+                },
               ),
             ),
+            Padding(
+              padding: EdgeInsets.all(15),
+              child: TextFormField(
+                controller: player2Controller,
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.white,
+                    ),
+                  ),
+                  focusedErrorBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: const Color.fromARGB(255, 210, 7, 7),
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.white,
+                    ),
+                  ),
+                  errorBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: const Color.fromARGB(255, 210, 7, 7),
+                    ),
+                  ),
+                  hintText: "Player 2 Name",
+                  hintStyle: TextStyle(color: Colors.white),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Plases Enter Player 2 Name ";
+                  }
+                  return null;
+                },
+              ),
+            ),
+            // Start Game Butoon and All detiles
+            SizedBox(height: 20),
+            InkWell(
+              onTap: () {},
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                padding: EdgeInsets.symmetric(vertical: 17, horizontal: 20),
+                child: Text(
+                  "Start Game",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            )
           ],
         ),
       ),
